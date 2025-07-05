68a329d0af4666e20679c0a7def49a2eeb01a26a
Son Juwan <wndhks171@naver.com>
2025-07-06 01:23:33 +0900

    #18- mypage UI

--- app/src/main/java/umc/hackathon/presentation/ui/main/mypage/MypageScreen.kt
+++ app/src/main/java/umc/hackathon/presentation/ui/main/mypage/MypageScreen.kt
@@ -1,31 +1,30 @@
 package umc.hackathon.presentation.ui.main.mypage
 
+import JobPager
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
-import androidx.compose.material3.TabRowDefaults
-import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import kotlinx.collections.immutable.toPersistentList
+import umc.hackathon.core.component.ActionBar
+import umc.hackathon.core.designsystem.theme.UMCHackathonTheme
+import umc.hackathon.presentation.ui.main.mypage.component.MypageHeader
+import umc.hackathon.presentation.ui.main.mypage.component.SettingSheet
 
 @Composable
 fun MypageScreen(
@@ -37,73 +36,72 @@ fun MypageScreen(
     Column(
         modifier = Modifier
             .padding(paddingValues)
+            .background(color = UMCHackathonTheme.colorScheme.white)
             .fillMaxSize()
     ) {
 //        Text("Mypage 화면입니다")
 //        Button(onClick = { navController.navigate(MainTabRoute.Search.route) }) {
 //            Text("검색으로 이동")
 //        }
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-
-        ) {
-            Text(
-                text = "000 님"
-
-            )
-            Spacer(modifier = Modifier.weight(1F))
+        ActionBar()
+        MypageHeader(name = "신하람", info = "시각장애·청각장애")
 
 
-        }
 
-        Spacer(modifier = Modifier.height(24.dp))
+            Spacer(modifier = Modifier.height(12.dp))
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = UMCHackathonTheme.colorScheme.gray100)
 
-        TabRow(
-            selectedTabIndex = 0,
-            modifier = Modifier.fillMaxWidth(),
-            containerColor = Color.LightGray,
-            contentColor = Color.Black,
-            indicator = { tabPositions ->
-                TabRowDefaults.SecondaryIndicator(
-                    modifier = Modifier.tabIndicatorOffset(
-                        tabPositions[selectedIndex.value]
-                    ),
-                    color = Color.DarkGray
-                )
+            ) {
+                item {
+                    Text(
+                        text = "나의 지원 내역",
+                        color = UMCHackathonTheme.colorScheme.black,
+                        modifier = Modifier.padding(start = 16.dp, top = 16.dp),
+                        style = UMCHackathonTheme.typography.Bold
 
-            }
-        ) {
-            contents.forEachIndexed { index, content ->
-                Tab(
-                    selected = selectedIndex.value == index,
-                    onClick = { selectedIndex.value = index },
-                    modifier = Modifier.padding(8.dp)
+                    )
+                }
+                item {
+                    JobPager()
+                }
+                item{
+                    Text(
+                        text = "설정",
+                        color = UMCHackathonTheme.colorScheme.black,
+                        modifier = Modifier.padding(start = 16.dp, top = 16.dp),
+                        style = UMCHackathonTheme.typography.Bold
 
-                ) {
-                    Text(text = "content ${content}")
+                    )
+                    Spacer(modifier = Modifier.height(12.dp))
                 }
 
-
+                item{
+                    Box(
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp)
+                    ){
+                        SettingSheet()
+                    }
+                }
             }
 
-        }
-        LazyColumn(
-            modifier = Modifier
-                .background(color =Color.Gray)
-                .fillMaxWidth()
-                .fillMaxHeight()
-        ){
 
-        }
+
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun showMypage(){
-    MypageScreen(
-        paddingValues = PaddingValues(),
-        navController = NavController(LocalContext.current)
-    )
+fun showMypage() {
+    UMCHackathonTheme {
+        MypageScreen(
+            paddingValues = PaddingValues(),
+            navController = NavController(LocalContext.current)
+        )
+
+    }
+
 }
