289af3d9390f6257fe134298212fa6b85e18bf36
Son Juwan <wndhks171@naver.com>
2025-07-05 21:43:36 +0900

    #11- presentation/community

---
+++ app/src/main/java/umc/hackathon/presentation/ui/main/community/CommunityScreen.kt
@@ -0,0 +1,13 @@
+package umc.hackathon.presentation.ui.main.community
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun CommunityScreen(
+    paddingValues: PaddingValues,
+    navController: NavController,
+){
+
+}
