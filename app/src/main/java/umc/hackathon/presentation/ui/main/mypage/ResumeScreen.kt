637697dcb93dfb6e16a6f1ab6b0b08d8a0648659
Son Juwan <wndhks171@naver.com>
2025-07-05 22:36:21 +0900

    #14- mypage/resume 화면 생성

---
+++ app/src/main/java/umc/hackathon/presentation/ui/main/mypage/ResumeScreen.kt
@@ -0,0 +1,20 @@
+package umc.hackathon.presentation.ui.main.mypage
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+
+
+@Composable
+fun  ResumeScreen(
+    paddingValues: PaddingValues,
+    navController: NavController,
+    viewModel: ResumeViewModel = hiltViewModel()
+){
+    Column (
+
+    ){  }
+
+}
