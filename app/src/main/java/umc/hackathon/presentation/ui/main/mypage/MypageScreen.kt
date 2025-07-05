cf2fd682de898b010699cfb05281b8cdd9e8b51e
Son Juwan <wndhks171@naver.com>
2025-07-06 06:06:08 +0900

    #31- presentation 수정

--- app/src/main/java/umc/hackathon/presentation/ui/main/mypage/MypageScreen.kt
+++ app/src/main/java/umc/hackathon/presentation/ui/main/mypage/MypageScreen.kt
@@ -2,19 +2,11 @@ package umc.hackathon.presentation.ui.main.mypage
 
 import JobPager
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,8 +15,10 @@ import androidx.navigation.NavController
 import kotlinx.collections.immutable.toPersistentList
 import umc.hackathon.core.component.ActionBar
 import umc.hackathon.core.designsystem.theme.UMCHackathonTheme
+import umc.hackathon.presentation.ui.main.mypage.component.AddButton
 import umc.hackathon.presentation.ui.main.mypage.component.MypageHeader
 import umc.hackathon.presentation.ui.main.mypage.component.SettingSheet
+import umc.hackathon.presentation.ui.main.mypage.navigation.MypageNavigation
 
 @Composable
 fun MypageScreen(
@@ -33,27 +27,25 @@ fun MypageScreen(
 ) {
     val selectedIndex = remember { mutableStateOf(0) }
     val contents = listOf("1", "2").toPersistentList()
-    Column(
+
+    val mypageNavigation = MypageNavigation()
+
+    Box(
         modifier = Modifier
+            .fillMaxSize()
             .padding(paddingValues)
             .background(color = UMCHackathonTheme.colorScheme.white)
-            .fillMaxSize()
     ) {
-//        Text("Mypage 화면입니다")
-//        Button(onClick = { navController.navigate(MainTabRoute.Search.route) }) {
-//            Text("검색으로 이동")
-//        }
-        ActionBar()
-        MypageHeader(name = "신하람", info = "시각장애·청각장애")
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            ActionBar()
+            MypageHeader(name = "신하람", info = "시각장애·청각장애")
 
-
-
-            Spacer(modifier = Modifier.height(12.dp))
             LazyColumn(
                 modifier = Modifier
                     .fillMaxWidth()
                     .background(color = UMCHackathonTheme.colorScheme.gray100)
-
             ) {
                 item {
                     Text(
@@ -61,35 +53,44 @@ fun MypageScreen(
                         color = UMCHackathonTheme.colorScheme.black,
                         modifier = Modifier.padding(start = 16.dp, top = 16.dp),
                         style = UMCHackathonTheme.typography.Bold
-
                     )
                 }
                 item {
                     JobPager()
                 }
-                item{
+                item {
                     Text(
                         text = "설정",
                         color = UMCHackathonTheme.colorScheme.black,
                         modifier = Modifier.padding(start = 16.dp, top = 16.dp),
                         style = UMCHackathonTheme.typography.Bold
-
                     )
                     Spacer(modifier = Modifier.height(12.dp))
                 }
 
-                item{
+                item {
                     Box(
                         modifier = Modifier
                             .padding(horizontal = 16.dp)
-                    ){
+                    ) {
                         SettingSheet()
                     }
                 }
             }
+        }
 
-
-
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 20.dp, end = 16.dp)
+        ) {
+            AddButton(
+                isExpanded = selectedIndex.value == 0,
+                onClick = {
+                    mypageNavigation.navigateToResumeScreen(navController)
+                }
+            )
+        }
     }
 }
 
@@ -101,7 +102,5 @@ fun showMypage() {
             paddingValues = PaddingValues(),
             navController = NavController(LocalContext.current)
         )
-
     }
-
 }
